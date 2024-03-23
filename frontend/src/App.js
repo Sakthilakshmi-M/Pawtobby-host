28f4f1b43ccb524bb3d6d7a2d75de34b46e242bd
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2024-03-23 22:22:25 +0530

    Navigation url changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import {useAuthContext} from "./hooks/useAuthContext"
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
@@ -7,14 +8,15 @@ import SamplePage from './pages/SamplePage';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
+  const {user} = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<SignupPage />} />
+          <Route path="/login" element={!user?<LoginPage />:<Navigate to="/"/>} />
+          <Route path="/register" element={!user?<SignupPage />:<Navigate to="/"/>} />
           <Route path="/sample" element={<SamplePage />} />
         </Routes>
       </BrowserRouter>
