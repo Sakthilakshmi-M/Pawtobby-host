f21f180591740b26acc1e9783971feb6d111a82f
Sakthilakshmi-M <sakthilakshmims@gmail.com>
2024-03-31 13:03:39 +0530

    bug fix

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -11,6 +11,7 @@ const register = async(req,res)=>{
     const token = createToken(user._id);
     const url = `http://localhost:3000/verify/${user.email}/${token}`
     sendEmail({msg:url,email:email});
+    console.log(url)
     res.status(200).json({msg:"Email Sent Successfully"});
   }
   catch(error)
